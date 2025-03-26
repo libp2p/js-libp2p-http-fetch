7466502a51f87a2626a53fb0ad4607e06e7d716c
Alex Potsides <alex@achingbrain.net>
2025-03-26 09:58:59 +0100

    fix: replace @perseveranza-pets/milo with @achingbrain/http-parser-js (#51)
    `@achingbrain/http-parser-js` is a fork of `http-parser-js` that converts it to ESM and uses `Uint8Array`s instead of node `Buffer`s.  It is 6.6kb.
    
    `@perseveranza-pets/milo` is 84.7kb.
    
    Fixes #35

--- .aegir.js
+++ .aegir.js
@@ -1,5 +1,9 @@
-/** @type {import('aegir').PartialOptions} */
+
+/** @type {import('aegir/types').PartialOptions} */
 export default {
+  build: {
+    bundlesizeMax: '18kB'
+  },
   dependencyCheck: {
     ignore: [
       'undici' // required by http-cookie-agent
